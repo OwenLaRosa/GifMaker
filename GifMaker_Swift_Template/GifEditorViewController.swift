b0cddce5f8ad487a2213e2336286f8aeaa7349e5
OwenLaRosa <owen.larosa@outlook.com>
2016-05-16 18:30:13 -0400

    Add files for view controllers

---
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GifEditorViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Owen LaRosa on 5/16/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Cocoa
+
+class GifEditorViewController: UIViewController {
+
+}
---
+++ GifMaker_Swift_Template/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Owen LaRosa on 5/16/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
