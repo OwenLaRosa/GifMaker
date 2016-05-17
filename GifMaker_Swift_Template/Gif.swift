98939d70633c949be05ab37c3a10363f6a0d5cf2
OwenLaRosa <owen.larosa@outlook.com>
2016-05-17 17:52:15 -0400

    Add Gif class

---
+++ GifMaker_Swift_Template/Gif.swift
@@ -0,0 +1,25 @@
+//
+//  Gif.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Owen LaRosa on 5/17/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class Gif {
+    
+    let url: NSURL
+    let caption: String
+    let gifImage: UIImage
+    let rawVideoURL: NSURL
+    
+    init(url: NSURL, caption: String, gifImage: UIImage, rawVideoURL: NSURL) {
+        self.url = url
+        self.caption = caption
+        self.gifImage = gifImage
+        self.rawVideoURL = rawVideoURL
+    }
+    
+}
