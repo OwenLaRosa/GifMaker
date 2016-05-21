c2993dab82302ebe10f6c07af965b29d10ee42b9
OwenLaRosa <owen.larosa@outlook.com>
2016-05-21 19:17:11 -0400

    Allow selection of photos from library

--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -17,13 +17,44 @@ let loopCount = 0 // loop infinitely
 
 extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBAction func launchVideoCamera(sender: AnyObject) {
+    @IBAction func presentVideoOptions(sender: AnyObject) {
+        if !UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            launchPhotoLibrary()
+        } else {
+            let actionSheet = UIAlertController(title: "Create new GIF", message: nil, preferredStyle: .ActionSheet)
+            let recordVideo = UIAlertAction(title: "Record a Video", style: .Default, handler: { (UIAlertAction) in
+                self.launchVideoCamera()
+            })
+            let chooseFromExisting = UIAlertAction(title: "Choose from Existing", style: .Default, handler: { (UIAlertAction) in
+                self.launchPhotoLibrary()
+            })
+            let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            
+            actionSheet.addAction(recordVideo)
+            actionSheet.addAction(chooseFromExisting)
+            actionSheet.addAction(cancel)
+            
+            actionSheet.view.tintColor = UIColor(red: 255.0/255.0, green: 65.0/255.0, blue: 112.0/255.0, alpha: 1.0)
+            
+            presentViewController(actionSheet, animated: true, completion: nil)
+        }
+    }
+    
+    func launchVideoCamera() {
         let recordVideoController = UIImagePickerController()
         recordVideoController.sourceType = .Camera
         recordVideoController.mediaTypes = [kUTTypeMovie as String]
         recordVideoController.allowsEditing = false
         recordVideoController.delegate = self
         presentViewController(recordVideoController, animated: true, completion: nil)
+    }
+    
+    func launchPhotoLibrary() {
+        let chooseExistingController = UIImagePickerController()
+        chooseExistingController.sourceType = .PhotoLibrary
+        chooseExistingController.allowsEditing = false
+        chooseExistingController.delegate = self
+        presentViewController(chooseExistingController, animated: true, completion: nil)
     }
     
     public func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
