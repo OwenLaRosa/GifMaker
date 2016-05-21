8ddc628eea059170786908bd10afdef4e82aac8b
OwenLaRosa <owen.larosa@outlook.com>
2016-05-21 18:59:04 -0400

    Add outlets, display gif, implement sharing

--- GifMaker_Swift_Template/PreviewViewController.swift
+++ GifMaker_Swift_Template/PreviewViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class PreviewViewController: UIViewController {
     
+    @IBOutlet weak var gifImageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
     var gif: Gif!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        gifImageView.image = gif.gifImage
+    }
+    
+    @IBAction func shareGif(sender: AnyObject) {
+        let animatedGif = NSData(contentsOfURL: gif.url)!
+        let itemsToShare = [animatedGif]
+        let shareController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        shareController.completionWithItemsHandler = {activityType, completed, returnedItems, activityError in
+            if completed {
+                self.navigationController?.popToRootViewControllerAnimated(true)
+            }
+        }
+        presentViewController(shareController, animated: true, completion: nil)
+    }
     
 }
