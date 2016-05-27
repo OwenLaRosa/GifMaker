68b8ba39acce5fd7df1e2a0cd272fc3c5e55ed49
OwenLaRosa <owen.larosa@outlook.com>
2016-05-27 12:04:08 -0400

    Create SavedGifsViewController and set as root

---
+++ GifMaker_Swift_Template/SavedGifsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SavedGifsViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Owen LaRosa on 5/27/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class SavedGifsViewController: UIViewController {
+    
+    private var gifs = [Gif]()
+    
+}
