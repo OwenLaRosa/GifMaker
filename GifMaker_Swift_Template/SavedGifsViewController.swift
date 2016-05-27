a257bb39d39c9a43a57e092501a8cba80ef968bc
OwenLaRosa <owen.larosa@outlook.com>
2016-05-27 12:30:42 -0400

    Implement data source, delegate, and flow layout

--- GifMaker_Swift_Template/SavedGifsViewController.swift
+++ GifMaker_Swift_Template/SavedGifsViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class SavedGifsViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var emptyView: UIStackView!
+    
     private var gifs = [Gif]()
+    let cellMargin: CGFloat = 8.0
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emptyView.hidden = gifs.count > 0
+        collectionView.reloadData()
+    }
+    
+}
+
+extension SavedGifsViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gifs.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("GifCell", forIndexPath: indexPath) as! GifCell
+        let gif = gifs[indexPath.row]
+        cell.configureForGif(gif)
+        return cell
+    }
+    
+}
+
+extension SavedGifsViewController: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+}
+
+extension SavedGifsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width = (collectionView.frame.size.width - cellMargin * 3.0)/2
+        return CGSizeMake(width, width)
+    }
     
 }
